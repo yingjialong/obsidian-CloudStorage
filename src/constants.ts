bf47a5be8f3a840bca7bd11ccd9d7e276f49eac2
yingjialong <majiajun@gmail.com>
2024-12-24 22:54:34 +0800

    Code refactoring

--- src/constants.ts
+++ src/constants.ts
@@ -6,10 +6,10 @@ export const PART_MAX_RETRIES = 3;
 export const DEFAULT_MAX_UPLOAD_SIZE = 5 * 1024 * 1024;
 export const DEFAULT_MAX_UPLOAD_SIZE_AUTO = 20;
 export const DEFAULT_PASSWORD = "Obcs_88py";
-// export const LINK_BASE_URL = "https://link.obcs.top";
-// export const USER_MANAGER_BASE_URL = 'https://obcs-api.obcs.top/api';
-export const LINK_BASE_URL = "http://127.0.0.1:5002";
-export const USER_MANAGER_BASE_URL = 'http://127.0.0.1:5001/api';
+export const LINK_BASE_URL = "https://link.obcs.top";
+export const USER_MANAGER_BASE_URL = 'https://obcs-api.obcs.top/api';
+// export const LINK_BASE_URL = "http://127.0.0.1:5002";
+// export const USER_MANAGER_BASE_URL = 'http://127.0.0.1:5001/api';
 // export const USER_MANAGER_BASE_URL = 'https://dev.wor1d.top/api';
 
 export const DEFAULT_SETTINGS: CloudStorageSettings = {
